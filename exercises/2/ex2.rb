299dc185f901f8e4aec69368e92a8ceba85b1b2f
Joshua Keroes <jkeroes@gmail.com>
2013-01-22 22:50:08 -0800

    extra credit complete

--- exercises/2/ex2.rb
+++ exercises/2/ex2.rb
@@ -1,8 +1,30 @@
-# blah blah
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by Ruby.
 
-puts "I could have code like this." # and the comment after is ignored.
+puts "I could have code like this." # and the comment after is ignored
 
-# Commented out code:
+# You can also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run."
 
 puts "This will run."
+
+# Extra Credit 1: Find out if you were right about what the character does and
+# make sure you know what it's called (octothorpe or pound character).
+#
+# => yes.
+
+# Extra Credit 2: Take your ex2.rb file and review each line going backwards.
+# Start at the last line, and check each word in reverse against what you
+# should have typed.
+#
+# => done.
+
+# Extra Credit 3: Did you find more mistakes? Fix them.
+#
+# => There were no code errors.
+# => I updated the comments to match.
+
+# Extra Credit 4: Read what you typed above out loud, including saying each
+# character by its name. Did you find more mistakes? Fix them.
+#
+# => done.
