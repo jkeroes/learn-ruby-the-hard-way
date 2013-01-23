9992a6d29efeeb207ebce8e6aebab6c8f5bc9f3f
Joshua Keroes <jkeroes@gmail.com>
2013-01-22 22:43:09 -0800

    extra credit complete

--- exercises/1/ex1.rb
+++ exercises/1/ex1.rb
@@ -1,7 +1,25 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I lke typing this."
-puts "This is fun."
-puts 'Yay! Printing.'
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I lke typing this."
+# puts "This is fun."
+# puts 'Yay! Printing.'
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+
+# Extra credit 1. Make your script print another line.
+#
+puts 'sure.'
+
+# Extra credit 2: Make your script print only one of the lines.
+#
+# => commented out everything.
+
+# Extra credit 3: Put a # (octothorpe) character at the beginning 
+# of a line. What did it do? Try to find out what this character does.
+#
+# => It starts a comment. The comment ends at the EOL.
+
+# Extra credit 3: From now on, I won't explain how each exercise 
+# works unless an exercise is different.
+#
+# => noted
