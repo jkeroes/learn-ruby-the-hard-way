8537ef875c0828d46d52afa5eb98658f9205dab3
Joshua Keroes <jkeroes@gmail.com>
2013-01-23 22:18:47 -0800

    done. extra credit complete, too.

---
+++ exercises/9/ex9.rb
@@ -0,0 +1,19 @@
+# Here's some strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here.
+With this PARAGRAPH thing
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
+
+# Extra Credit 1: Do your checks of your work, write down your mistakes, try
+# not to make them on the next exercise.
+#
+# => done.
