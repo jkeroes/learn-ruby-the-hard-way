b427981adc8e9ef8b944226102a5af73780e1662
Joshua Keroes <jkeroes@gmail.com>
2013-01-25 20:09:42 -0800

    Extra credit complete

--- exercises/14/ex14.rb
+++ exercises/14/ex14.rb
@@ -1,13 +1,16 @@
-user = ARGV.first
-prompt = '> '
+# encoding: utf-8
 
-puts "Hi, #{user}, I'm the #{$0} script."
+first_name, last_name = ARGV
+
+prompt = '⮀ '
+
+puts "#{first_name} #{last_name}, I'm the #{$0} script."
 puts "I'd like to ask you a few questions."
-puts "Do you like me, #{user}?"
+puts "Do you like me, #{first_name}?"
 print prompt
 likes = STDIN.gets.chomp()
 
-puts "Where do you live, #{user}?"
+puts "Where do you live, #{first_name}?"
 print prompt
 lives = STDIN.gets.chomp()
 
@@ -21,3 +24,21 @@ You live in #{lives}. Not sure where that is.
 And you have a #{computer} computer. Nice.
 MESSAGE
 
+# Extra Credit 1: Find out what Zork and Adventure were. Try to find a copy
+# and play it.
+#
+# => Been there, done that.
+
+# Extra Credit 1: Change the prompt variable to something else entirely.
+#
+# => Tried a unicode char but ruby balked with this error: "invalid multibyte
+# => char (US-ASCII)". Added an encoding comment and ruby's happy.
+
+# Extra Credit 1: Add another argument and use it in your script.
+#
+# => done
+
+# Extra Credit 1: Make sure you understand how I combined a <<SOMETHING style
+# multi-line string with #{ } string interpolation as the last print.
+#
+# => yup
