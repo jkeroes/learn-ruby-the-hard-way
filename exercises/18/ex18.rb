4b48fc74e33c7d4507cad43a7eaf22437c329883
Joshua Keroes <jkeroes@gmail.com>
2013-01-26 17:48:12 -0800

    extra credit complete

--- exercises/18/ex18.rb
+++ exercises/18/ex18.rb
@@ -24,3 +24,33 @@ puts_two_again("Zed", "Shaw")
 puts_one("First!")
 puts_none()
 
+# Extra Credit
+#
+# Write out a function checklist for later exercises. Write these on an index
+# card and keep it by you while you complete the rest of these exercises or
+# until you feel you do not need it:
+#
+# * Did you start your function definition with def?
+# * Does your function name have only characters and _ (underscore) characters?
+# * Did you put an open parenthesis ( right after the function name?
+# * Did you put your arguments after the parenthesis ( separated by commas?
+# * Did you make each argument unique (meaning no duplicated names).
+# * Did you put a close parenthesis ) after the arguments?
+# * Did you indent all lines of code you want in the function 2 spaces?
+# * Did you close your function body by typing "end"?
+#
+# => noted (though I prefer 4 space intdentation; to each their own)
+
+# And when you run (aka "use" or "call") a function, check these things:
+#
+# * Did you call/use/run this function by typing its name?
+# * Did you put ( character after the name to run it? (this isn't required,
+#   but is idiomatic)
+# * Did you put the values you want into the parenthesis separated by commas?
+# * Did you end the function call with a ) character.
+# * Use these two checklists on the remaining lessons until you do not need
+#   them anymore.
+# * Finally, repeat this a few times:
+#     "To 'run', 'call', or 'use' a function all mean the same thing."
+#
+# => noted.
