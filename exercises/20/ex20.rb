56c7bdaa5e6902f29a0e02b05c82be63ec1fccfb
Joshua Keroes <jkeroes@gmail.com>
2013-01-26 18:32:49 -0800

    extra credit complete

--- exercises/20/ex20.rb
+++ exercises/20/ex20.rb
@@ -34,3 +34,30 @@ print_a_line(current_line, current_file)
 current_line = current_line + 1
 print_a_line(current_line, current_file)
 
+# Extra Credit 1: Go through and write English comments for each line to
+# understand what's going on.
+#
+# => pass.
+
+# Extra Credit 2: Each time print_a_line is run you are passing in a variable
+# current_line. Write out what current_line is equal to on each function call,
+# and trace how it becomes line_count in print_a_line.
+#
+# => done.
+
+# Extra Credit 3: Find each place a function is used, and go check its def to
+# make sure that you are giving it the right arguments.
+#
+# => done.
+
+# Extra Credit 4: Research online what the seek function for file does. Look
+# at the rdoc documentation using the ri command and see if you can figure it
+# out from there.
+#
+# => done.
+
+# Extra Credit 5: Research the shorthand notation += and rewrite the script to
+# use that.
+#
+# => 
+
