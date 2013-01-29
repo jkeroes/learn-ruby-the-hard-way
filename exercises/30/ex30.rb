9646ee3735c67af60a5abf12aa72518276c59fb6
Joshua Keroes <joshua.keroes@integratelecom.com>
2013-01-28 18:44:42 -0800

    extra credit complete

--- exercises/30/ex30.rb
+++ exercises/30/ex30.rb
@@ -1,6 +1,6 @@
 people = 30
 cars = 40
-buses = 15
+buses = 50
 
 if cars > people
   puts "We should take the cars."
@@ -22,4 +22,28 @@ if people > buses
   puts "Alright, let's just take the buses."
 else
   puts "Fine, let's stay home then."
+end
+
+# Extra Credit 1: Try to guess what elsif and else are doing.
+#
+# ==> the first expr in an if or elsif "wins" and its block will get executed.
+# ==> if they all fail, the else clause will get executed instead.
+
+# Extra Credit 2: Change the numbers of cars, people, and buses and then
+# trace through each if-statement to see what will be printed.
+#
+# ==> done.
+
+# Extra Credit 3: Try some more complex boolean expressions like cars >
+# people and buses < cars. Above each line write an English description of
+# what the line does.
+#
+# ==> done. Skipping the comments; this code is sely-documenting.
+
+if buses < cars
+  puts "Sounds like 'Murica"
+end
+
+if cars > people
+  puts "Detroit's dream"
 end
