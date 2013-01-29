4d6692b067606e621a5cd016353d92d44ab24e53
Joshua Keroes <joshua.keroes@integratelecom.com>
2013-01-28 18:42:40 -0800

    extra credit complete

--- exercises/29/ex29.rb
+++ exercises/29/ex29.rb
@@ -31,3 +31,28 @@ end
 if people == dogs
   puts "People are dogs."
 end
+
+# Extra Credit 1: What do you think the if does to the code under it?
+#
+# ==> if runs the indented code block if the expression evaluates to true.
+
+# Extra Credit 2: Can you put other boolean expressions from Ex. 27 in the
+# if-statement? Try it.
+
+if people.equal?(dogs)
+  puts "people is .equal to dogs"
+end
+
+if people.eql?(dogs)
+  puts "people is .eql to dogs"
+end
+
+if (people <=> dogs) == 0
+  puts "people <=> dogs evaluated to 0"
+end
+
+# Extra Credit 3: What happens if you change the initial variables for
+# people, cats, and dogs?
+#
+# ==> different strings are printed.
+
